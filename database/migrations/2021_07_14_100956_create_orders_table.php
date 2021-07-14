66724f5a1e697318fbb1e340bda4230187f3a483
rajifafif <rajifafif@gmail.com>
2021-07-14 17:47:15 +0700

    Prepare Food, FoodOrder, Order, Table Migration and Seeder

---
+++ database/migrations/2021_07_14_100956_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_nbr')->unique();
+            $table->unsignedBigInteger('table_id');
+            $table->string('status');
+            $table->decimal('total_price', 12,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/seeders/FoodSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Food;
+use App\Models\Table;
+use Illuminate\Database\Seeder;
+
+class FoodSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $foods = [
+            [
+                'name' => 'Nasi Goreng',
+                'type' => 'makanan',
+                'price' => 20000
+            ],
+            [
+                'name' => 'Es Jeruk',
+                'type' => 'minuman-dingin',
+                'price' => 5000
+            ],
+            [
+                'name' => 'Kentang Goreng',
+                'type' => 'cemilan',
+                'price' => 10000
+            ],
+
+        ];
+
+        foreach ($foods as $food) {
+            Food::updateOrCreate(['name' => $food['name']], $food);
+        }
+
+        for ($x=1; $x<=10; $x++) {
+            Table::updateOrCreate(['name' => 'Meja '. $x], [
+                'name' => 'Meja '. $x,
+                'seat' => 4
+            ]);
+        }
+    }
+}
