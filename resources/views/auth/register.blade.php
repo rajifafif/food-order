3f38376f144a69c81f3fdc25eb404bdbc16810ce
rajifafif <rajifafif@gmail.com>
2021-07-28 16:00:33 +0700

    Add Manifest to All Login

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,10 @@
 @extends('adminlte::auth.register')
+@push('css')
+    <meta name="theme-color" content="#000">
+    <link rel="manifest" href="{{ asset('manifest.json') }}">
+    <link rel="apple-touch-icon" href="{{ asset('vendor/adminlte/dist/img/AdminLTELogo.png') }}">
+@endpush
+
+@push('js')
+    <script src='{{ asset('js/app.js') }}'></script>
+@endpush
